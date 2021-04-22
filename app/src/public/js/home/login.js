02c98da108dab07cd8709ede5582434ab14375cb
hojin <cacabi24@hanmail.net>
2021-04-22 14:44:17 +0900

    Backend login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,6 +12,14 @@ function login() {
         psword: psword.value
     };
 
-    console.log(req);
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,43 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const users = {
+    id: ["coco", "caca"],
+    psword: ["1234", "5678"]
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    }
 };
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+          psword = req.body.psword;
+
+    if ( users.id.includes(id)) {
+        const idx = users.id.indexOf(id);
+        if (users.psword[idx] === psword) {
+            return res.json({
+                success: true
+            });
+        }
+    }
+
+    return res.json({
+        success: false,
+        msg: "로그인에 실패하였습니다."
+    });
+
+    }
+}
 
 module.exports = {
-    home,
-    login,
+    output,
+    process
 
 }
