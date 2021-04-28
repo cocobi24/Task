fbdf7dd5bdae0bd0f9944fad55d11de27ae85320
hojin <cacabi24@hanmail.net>
2021-04-28 13:13:43 +0900

    세션 작업

--- app/app.js
+++ app/app.js
@@ -3,6 +3,14 @@
 //모듈
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const session = require('express-session');
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const FileStore = require('session-file-store')(session); // 세션을 파일에 저장
+const cookieParser = require('cookie-parser');
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -17,6 +25,14 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'DevT',
+    resave: false,
+    saveUninitialized: true,
+    store : new FileStore()
+      })
+  );
 
 //URL을 통해 전달되는 데이터에 한글,공백과 같은 문자가 포함된 경우 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended :true }));
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -2,11 +2,24 @@
 
 const logger = require("../../config/logger");
 const User = require("../../models/User");
+const session = require('express-session');
+const client = require('../../config/db');
 
 const output = {
     home: (req, res) => {
         logger.info(`GET / 304 "홈 화면으로 이동"`);
-        res.render("home/index");
+        if(req.session.is_logined == true){
+            res.render('home/index',{
+                is_logined : req.session.is_logined,
+                name : req.session.name
+            });
+        }else{
+            res.render('home/index',{
+                is_logined : false
+            });
+        }
+    
+        // res.render("home/index");
     },
 
     login: (req, res) => {
@@ -17,6 +30,15 @@ const output = {
     register: (req, res) => {
         logger.info(`GET /register 304 "회원가입 화면으로 이동"`);
         res.render("home/register");
+    },
+
+    logout: (req, res) => {
+        logger.info(`GET /logout 304 "로그아웃 화면으로 이동"`);
+        console.log(req.session);
+   
+        req.session.destroy(function(err){
+            res.redirect('/');
+          });
     }
 };
 
@@ -30,6 +52,39 @@ const process = {
             path: "/login",
             status: response.err ? 400 : 200
         };
+
+        const body = req.body;
+        const id = body.id;
+        const pw = body.pw;
+        const name = body.name
+
+        client.query('select * from users where id=?',[id],(err,data)=>{
+            // 로그인 확인
+            console.log(data[0]);
+            console.log(id);
+            console.log(data[0].id);
+            console.log(data[0].psword);
+            console.log(id == data[0].id);
+            console.log(pw == data[0].psword);
+            if(id == data[0].id || pw == data[0].psword){
+                console.log('로그인 성공');
+                // 세션에 추가
+                req.session.is_logined = true;
+                req.session.name = data.name;
+                // req.session.id = data.id;
+                req.session.pw = data.psword;
+                req.session.save(function(){ // 세션 스토어에 적용하는 작업
+                    res.render('home/index',{ // 정보전달
+                        name : data[0].name,
+                        id : data[0].id,
+                        is_logined : true
+                    });
+                });
+            }else{
+                console.log('로그인 실패');
+                res.render('login');
+            }
+        });
 
         log(response, url);
         return res.status(url.status).json(response);
